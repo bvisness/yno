403fbd2b20df69995fb46b9f1c1a31e192919f76
Ben Visness <ben@bvisness.me>
2023-04-15 14:56:48 -0500

    Dunno, WIP

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"io"
 	"net"
+	"net/http"
 	"net/url"
 	"os"
 
@@ -17,15 +19,20 @@ func init() {
 		Short: "y no server ??",
 		Run: func(cmd *cobra.Command, args []string) {
 			if len(args) < 1 {
-				fmt.Fprintf(os.Stderr, "ERROR: You must provide a URL.\n\n")
+				fmt.Printf("ERROR: You must provide a URL.\n\n")
 				RootCmd.Usage()
 				os.Exit(1)
 			}
 			urlStr := args[0]
 
 			u := parse(urlStr)
+			checks := runChecks(u)
 
-			fmt.Printf("Ok I will look up %v\n", u)
+			fmt.Println()
+			fmt.Println("Final report:")
+			fmt.Printf("%s Hostname of %s is valid and can be resolved by DNS\n", checko(checks.hostOK), u.Hostname())
+			fmt.Printf("%s DNS records for %s lead to this server\n", checko(checks.dnsMatches), u.Hostname())
+			fmt.Printf("%s TCP connections can be established on port %s\n", checko(checks.tcpWorks), u.Port())
 		},
 	}
 }
@@ -39,8 +46,6 @@ func parse(urlStr string) *url.URL {
 			host = u.Hostname()
 			port = u.Port()
 		}
-	} else {
-		fmt.Fprintf(os.Stderr, "Could not parse URL: %v\n", err)
 	}
 
 	if host == "" {
@@ -49,18 +54,12 @@ func parse(urlStr string) *url.URL {
 				host = nethost
 				port = netport
 			}
-		} else {
-			fmt.Fprintf(os.Stderr, "Could not parse host and port: %v\n", err)
 		}
 	}
 
 	if host == "" {
 		host = urlStr // what else can we do?
 	}
-
-	fmt.Printf("Scheme: %v\n", scheme)
-	fmt.Printf("Host: %v\n", host)
-	fmt.Printf("Port: %v\n", port)
 
 	result := &url.URL{
 		Scheme: scheme,
@@ -83,4 +82,116 @@ func validPort(port string) bool {
 		}
 	}
 	return true
+}
+
+func getExternalIP() (net.IP, error) {
+	res, err := http.Get("https://api64.ipify.org")
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	ipStrBytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return net.ParseIP(string(ipStrBytes)), nil
+}
+
+type Check int
+
+const (
+	CheckUnknown Check = iota
+	CheckSuccess
+	CheckFail
+	CheckWarn
+)
+
+type Checks struct {
+	hostOK     Check
+	dnsMatches Check
+	tcpWorks   Check
+	udpWorks   Check
+	icmpWorks  Check
+}
+
+func runChecks(u *url.URL) Checks {
+	var res Checks
+
+	fmt.Printf("Looking up host %s...\n", u.Hostname())
+	hostAddrs, err := net.LookupHost(u.Hostname())
+	if err != nil {
+		fmt.Printf("ERROR: could not look up host %s: %v\n", u.Hostname(), err)
+		res.hostOK = CheckFail
+		return res
+	}
+	res.hostOK = CheckSuccess
+
+	isLoopback := net.ParseIP(hostAddrs[0]).IsLoopback()
+	if !isLoopback {
+		fmt.Println("Looking up external IP address via ipify.org...")
+		externalIP, err := getExternalIP()
+		if err != nil {
+			fmt.Printf("ERROR: failed to get external IP address: %v\n", err)
+			return res
+		}
+
+		for _, addrString := range hostAddrs {
+			addr := net.ParseIP(addrString)
+			if externalIP.Equal(addr) {
+				res.dnsMatches = CheckSuccess
+			}
+		}
+
+		if res.dnsMatches != CheckSuccess {
+			fmt.Printf("POTENTIAL PROBLEM! None of the addresses for %s matched your external IP of %s.\n", u.Hostname(), externalIP)
+			res.dnsMatches = CheckWarn
+		}
+	}
+
+	if u.Scheme == "" {
+		fmt.Println("No protocol was given; assuming TCP")
+		u.Scheme = "tcp"
+	}
+
+	switch u.Scheme {
+	case "tcp", "http", "https":
+		// yay tcp, continue
+	default:
+		fmt.Printf("I do not understand what %s is because I am a jam project :)\n", u.Scheme)
+		return res
+	}
+
+	if u.Port() == "" {
+		fmt.Println("No port was given; assuming port 80")
+		u.Host = net.JoinHostPort(u.Host, "80")
+	}
+
+	fmt.Printf("Getting a TCP connection to %s...\n", u.Host)
+	conn, err := net.Dial("tcp", u.Host)
+	if err != nil {
+		fmt.Printf("ERROR: Could not establish TCP connection: %v\n", err)
+		res.tcpWorks = CheckFail
+		return res
+	}
+	defer conn.Close()
+
+	fmt.Println("Got a TCP connection.")
+	res.tcpWorks = CheckSuccess
+
+	return res
+}
+
+func checko(c Check) string {
+	switch c {
+	case CheckSuccess:
+		return "✅"
+	case CheckFail:
+		return "❌"
+	case CheckWarn:
+		return "⚠️"
+	default:
+		return "❓"
+	}
 }
--- ideas.md
+++ ideas.md
@@ -11,3 +11,9 @@ Stuff what can be wrong:
 - incorrect use of virtual hosts...?
 - program behind reverse proxy is not running
 - program behind reverse proxy is on wrong port
+
+Stuff that messed me up in the past:
+
+- Amazon cloud firewall blocking traffice
+- FIRST simulator binding only on 127.0.0.1 and not ::1, and Godot connecting only to ::1
+    - Could you do a "diff" or "bisect" almost? This works, but this doesn't, that kind of thing
